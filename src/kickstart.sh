36d19194149f6a9933a01e6ef13c47ad3dd15eb2
Stanislav Yudin <stan@endlessinsomnia.com>
2018-08-29 11:38:50 +1000

    initial commit

---
+++ src/kickstart.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# query SSM parameters store for secrets and save files and environment variables
+
+ssm-bootstrap --environ /tmp/app_environ --root /app/
+[ -f /tmp/app_environ ] && . /tmp/app_environ
+
+exec "$@"
